91197bf32022010b20296466a65c5360ec766d94
Alex <janouser11@gmail.com>
2017-02-15 13:10:44 -0500

    Added exception handling

--- main.cpp
+++ main.cpp
@@ -7,8 +7,13 @@
 #include <valarray>
 #include <array>
 #include <vector>
+#include <iomanip>
 
 double getVectorSum(const std::vector<double> &vector);
+
+void showEachTransaction(const std::vector<double> &vector);
+
+double getBeginningBalance(double beginningBalance);
 
 using namespace std;
 
@@ -18,14 +23,16 @@ int main()
 
     double beginningBalance;
     double endingBalance;
-
-    cout << "Please input a beginning bank balance: $";
-    cin >> beginningBalance;
     char transactionType;
     bool loopCounter = true;
     vector<double>deposits;
     vector<double>checks;
     vector<double>withdrawals;
+
+    beginningBalance = getBeginningBalance(beginningBalance);
+
+
+
 
     do {
         cout << "\n*************************************\n";
@@ -66,15 +73,48 @@ int main()
     double sumOfChecks = getVectorSum(checks);
     double sumOfWithdrawals = getVectorSum(withdrawals);
     endingBalance = (beginningBalance + sumOfDeposits + sumOfChecks) - sumOfWithdrawals;
-    cout << "Beginning balance was: $" << beginningBalance;
+    cout << setprecision(2) << fixed;
+    cout << "Transaction Summary: ";
     cout << "\n*************************************\n";
-    cout << "Total amount of deposits: $" << sumOfDeposits << endl;
-    cout << "Total amount of checks: $" << sumOfChecks << endl;
-    cout << "Total amount of withdrawals: $" << sumOfWithdrawals;
+    cout << "Beginning balance was: $" << beginningBalance << endl;
+
+    cout << "Deposits:" << endl;
+    showEachTransaction(deposits);
+    cout << "Total: $" << sumOfDeposits << endl << endl;
+
+    cout << "Checks:" << endl;
+    showEachTransaction(checks);
+    cout << "Total: $" << sumOfChecks << endl << endl;
+
+    cout << "Withdrawals:" << endl;
+    showEachTransaction(withdrawals);
+    cout << "Total: $" << sumOfWithdrawals;
+
     cout << "\n*************************************\n";
     cout << "Ending balance is: $" << endingBalance;
 
+
     return 0;
+}
+
+double getBeginningBalance(double beginningBalance) {
+    cout << "Please input a beginning bank balance: $";
+    while (1) {
+        if (cin >> beginningBalance) {
+            break;
+        } else {
+            cout << "Invalid Input! Please input a numerical value.\n $";
+            cin.clear();
+            while (cin.get() != '\n') ;
+        }
+    }
+    return beginningBalance;
+}
+
+void showEachTransaction(const vector<double> &vector) {
+    for(auto& x : vector){
+        cout << "\t\t -- $" << x << endl << setprecision(2);
+    }
 }
 
 double getVectorSum(const vector<double> &vector) {
@@ -84,6 +124,8 @@ double getVectorSum(const vector<double> &vector) {
     }
     return sum;
 }
+
+
 
 
 
